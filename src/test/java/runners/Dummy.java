6df89374c0f447533dce605f9e99f9e62701e2eb
sqs-automation <gopinath.rajaram@sqs.com>
2018-08-01 18:34:57 +0530

    evening update

---
+++ src/test/java/runners/Dummy.java
@@ -0,0 +1,36 @@
+package runners;
+
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.runner.RunWith;
+
+import base.Test;
+import constants.Device;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features = {"src/test/resources/features"},
+        glue = {"stepdefinitions"},
+        plugin = {"com.cucumber.listener.ExtentCucumberFormatter:","pretty","json:target/json-output/Automation-Summary.json"},
+        tags={"@VirtualRegistration"},
+        monochrome=true
+)
+
+public class GetgoTestRunner extends Test {
+    @BeforeClass
+    public static void initialize(){
+        String deviceName=System.getProperty("Device");
+        if(deviceName==null){
+            deviceName=Device.TGSMOB2131;
+        }
+        setup(deviceName);
+    }
+
+    @AfterClass
+    public static void flush(){
+        teardown();
+    }
+}
